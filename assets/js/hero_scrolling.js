d0b39d6058ffce4f3e7255e285c971de1afacd1f
Ada Null <ada@worldenddisk.com>
2024-07-06 18:12:47 +0100

    scrolling red effect occurs on load

--- assets/js/hero_scrolling.js
+++ assets/js/hero_scrolling.js
@@ -1,8 +1,11 @@
+function set_red() {
+  var scrollRatio = $("#main").scrollTop() / $("#hero-window").height();
+  const maxRatio = 0.4;
+  var effectRatio = scrollRatio / maxRatio;
+  $("#hero-image-red").css("opacity", effectRatio);
+}
+
 $(() => {
-    $("#main").scroll(() => {
-      var scrollRatio = $("#main").scrollTop() / $("#hero-window").height();
-      const maxRatio = 0.4;
-      var effectRatio = scrollRatio / maxRatio;
-      $("#hero-image-red").css("opacity", effectRatio);
-    })
-  });
+  set_red();
+  $("#main").scroll(set_red);
+});
