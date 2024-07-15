bc36318a8b64b23c7d130752eecee4a3d2fec215
Ada Null <ada@worldenddisk.com>
2024-07-15 19:06:32 +0100

    ticker speed dependent on width and randomises animation start

---
+++ assets/js/ticker.js
@@ -0,0 +1,16 @@
+function setScrollTime(first = false) {
+    const speed = 40.0;
+    const tickerTexts = $('.ticker-text');
+    let textWidth = tickerTexts.first().width();
+    let scrollTime = textWidth / speed;
+    tickerTexts.css('--scroll-time', `${scrollTime}s`);
+    if (first == true) {
+        let delay = Math.random() * scrollTime;
+        tickerTexts.css('animation-delay', `-${delay}s`);
+    }
+}
+
+$(() => {
+    setScrollTime(true);
+    $(window).on("resize", setScrollTime);
+});
