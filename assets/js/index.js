70bd6ee2f9b479121f701f9c706ef64c44783ce0
Ada Null <ada@worldenddisk.com>
2025-02-20 15:18:49 +0000

    sidebar stays where it should be between loads

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
 import "./hero_scrolling";
 import "./mobile_sidebar";
 import "./ticker";
+import "./sidebar_scroll";
---
+++ assets/js/sidebar_scroll.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.querySelector(".sidebar");
+  const savedScrollTop = localStorage.getItem("sidebarScroll");
+  
+  if (savedScrollTop !== null) {
+    sidebar.scrollTop = parseInt(savedScrollTop, 10);
+    
+    requestAnimationFrame(() => {
+      document.documentElement.classList.remove('js-restore-scroll');
+    });
+  }
+  
+  window.addEventListener("beforeunload", () => {
+    localStorage.setItem("sidebarScroll", sidebar.scrollTop);
+  });
+});
