b1aeefff0a8b8adfcfab05beb05513d8a69c0726
Ada Null <ada@worldenddisk.com>
2025-02-26 13:14:56 +0000

    js for images on hover

---
+++ assets/js/linkPreview.js
@@ -0,0 +1,18 @@
+const linkPreviewImg = document.querySelector(".link-preview");
+
+document.querySelectorAll("a[data-link-preview-src]").forEach((link) => {
+  link.addEventListener("mouseenter", () => {
+    linkPreviewImg.src = link.getAttribute("data-link-preview-src");
+    linkPreviewImg.style.visibility = "visible";
+  });
+
+  link.addEventListener("mouseleave", () => {
+    linkPreviewImg.style.visibility = "hidden";
+  });
+});
+
+window.addEventListener("load", () => {
+  document.querySelectorAll("a[data-link-preview-src]").forEach((link) => {
+    new Image().src = link.getAttribute("data-link-preview-src");
+  });
+});
